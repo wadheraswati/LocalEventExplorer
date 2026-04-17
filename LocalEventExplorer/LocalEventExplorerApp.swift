83122bd3586337b6cbedd70dad300d734bb53bae
wadheraswati <wadheraswati@gmail.com>
2026-04-17 12:13:47 -0400

    Initial Commit

---
+++ LocalEventExplorer/LocalEventExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalEventExplorerApp.swift
+//  LocalEventExplorer
+//
+//  Created by Swati Seera on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct LocalEventExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalEventExplorerUITests/LocalEventExplorerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LocalEventExplorerUITestsLaunchTests.swift
+//  LocalEventExplorerUITests
+//
+//  Created by Swati Seera on 2026-04-17.
+//
+
+import XCTest
+
+final class LocalEventExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
